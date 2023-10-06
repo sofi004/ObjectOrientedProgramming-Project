acbcf2af1f6cd995a0401f671b26099169095618
joaotomas007 <joaotomasgomes@gmail.com>
2023-10-06 16:34:43 +0100

    firstcommit

---
+++ xxl/core/Spreadsheet.java
@@ -0,0 +1,32 @@
+package xxl.core;
+
+// FIXME import classes
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import xxl.core.exception.UnrecognizedEntryException;
+
+/**
+ * Class representing a spreadsheet.
+ */
+public class Spreadsheet implements Serializable {
+  @Serial
+  private static final long serialVersionUID = 202308312359L;
+  
+  // FIXME define attributes
+  // FIXME define contructor(s)
+  // FIXME define methods
+  
+  /**
+   * Insert specified content in specified address.
+   *
+   * @param row the row of the cell to change 
+   * param column the column of the cell to change
+   * @param contentSpecification the specification in a string format of the content to put
+   *        in the specified cell.
+   */
+  public void insertContent(int row, int column, String contentSpecification) throws UnrecognizedEntryException /* FIXME maybe add exceptions */ {
+    //FIXME implement method
+  }
+}
