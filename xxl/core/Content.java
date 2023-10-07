5a264b2f377a9fa3959ff089f853a7acfbeb0d68
joaotomas007 <joaotomasgomes@gmail.com>
2023-10-07 11:03:21 +0100

    con

---
+++ xxl/core/Content.java
@@ -0,0 +1,11 @@
+package ObjectOrientedProgramming-Project.xxl.core;
+
+public abstract class Content {
+    
+    public Literal getValue(){
+
+    }
+    public string getSring(){
+        
+    }
+}
