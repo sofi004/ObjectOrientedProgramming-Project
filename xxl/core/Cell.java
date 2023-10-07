f0eb0e351847a7a29fd851a3936a2a42ff88fcd3
ist1106194 <sophiapiteira@gmail.com>
2023-10-07 11:13:42 +0100

    ee

--- xxl/core/Cell.java
+++ xxl/core/Cell.java
@@ -3,10 +3,23 @@ package xxl.core;
 public class Cell {
     private int _column;
     private int _row;
+    private Content _content;
 
 
     public Cell(int row, int column){
         _column = column;
         _row = row;
+    }
+
+    public int getRow(){
+        return _row;
+    }
+
+    public int getCollumn(){
+        return _column;
+    }
+
+    public void insertContent(String text){
+        _content = text
     }
 }
