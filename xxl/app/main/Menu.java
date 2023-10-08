e9cbc2f8028a7b01e1e912185ac211b83d401f81
joaotomas007 <joaotomasgomes@gmail.com>
2023-10-08 13:19:17 +0100

    comentarios

--- xxl/app/main/Menu.java
+++ xxl/app/main/Menu.java
@@ -10,10 +10,10 @@ public class Menu extends pt.tecnico.uilib.menus.Menu {
   public Menu(Calculator receiver) {
     super(Label.TITLE, //
           new DoNew(receiver), //
-          new DoOpen(receiver), //
-          new DoSave(receiver), //
-          new DoOpenEditMenu(receiver), //
-          new DoOpenSearchMenu(receiver) //
+          new DoOpen(receiver), // loads the state of the app saves in a file
+          new DoSave(receiver), // saves the current state of the app in a file
+          new DoOpenEditMenu(receiver), // opens the edit menu of a spreadsheet
+          new DoOpenSearchMenu(receiver) // opens the search menu on a spreadsheet
           );
   }
 }
