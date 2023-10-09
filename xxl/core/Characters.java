728ed627cb7f94f77ecacaf5eab62cb86572a185
joaotomas007 <joaotomasgomes@gmail.com>
2023-10-09 11:34:57 +0100

    zz

--- xxl/core/Characters.java
+++ xxl/core/Characters.java
@@ -2,4 +2,9 @@ package xxl.core;
 
 public class Characters extends Literal{
     private int _value;
+
+    public String toString(){
+        String z = "Example";
+        return z;
+    }
 }
--- xxl/core/Numbers.java
+++ xxl/core/Numbers.java
@@ -2,4 +2,9 @@ package xxl.core;
 
 public class Numbers extends Literal{
     private int _value;
+
+    public String toString(){
+        String z = "Example";
+        return z;
+    }
 }
