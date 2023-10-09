e5a3c3e081dab1a7595c75e7f9beabfcaf65b224
joaotomas007 <joaotomasgomes@gmail.com>
2023-10-09 13:26:56 +0100

    pj

---
+++ xxl/core/Character.java
@@ -0,0 +1,10 @@
+package xxl.core;
+
+public class Character extends Literal{
+    private String _value;
+
+    public String toString(){
+        String z = "Example";
+        return z;
+    }
+}
