79a1678f6d304ae8036f0c8bfefec173d0d6c959
joaotomas007 <joaotomasgomes@gmail.com>
2023-10-09 11:36:35 +0100

    z

--- xxl/core/Reference.java
+++ xxl/core/Reference.java
@@ -3,4 +3,9 @@ package xxl.core;
 public class Reference extends Content{
     private int _row;
     private int _column;
+
+    public String toString(){
+        String z = "Example";
+        return z;
+    }
 }
