e58defefd7021521234ee77ea193feabdbe086df
ist1106194 <sophiapiteira@gmail.com>
2023-10-26 17:21:09 +0100

    ee

--- xxl/core/Reference.java
+++ xxl/core/Reference.java
@@ -2,24 +2,38 @@ package xxl.core;
 
 import java.io.Serializable;
 
-public class Reference extends Content implements Serializable{
+public class Reference extends Content implements Serializable, Observer{
     private int _row;
     private int _column;
     private Spreadsheet _spreadsheet;
+    private Cell _cell;
+    private Literal _value;
 
     public Reference(int row, int column, Spreadsheet spreadsheet){
         _row = row;
         _column = column;
         _spreadsheet = spreadsheet;
+        _cell = _spreadsheet.getCells().searchCell(row, column);
+        _cell.addObserver(this);
+        update();
     }
 
     @Override
     public String toString(){
-           return  _spreadsheet.searchCell(_row, _column).value() + "=" + String.valueOf(_row)+";"+String.valueOf(_column);
+           return  _cell.value() + "=" + String.valueOf(_row)+";"+String.valueOf(_column);
+    }
+
+    public Cell getCell(){
+        return _cell;
     }
 
     @Override
     Literal value(){
-        return _spreadsheet.searchCell(_row, _column).value();   
+        return _value;   
     }
+
+    public void update(){
+        _value = _cell.value();
+    }
+
 }
